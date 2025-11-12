50064357a5283fae2f342594803b5c960a003827
lEric-Wl <eric.wang887@gmail.com>
2025-11-11 21:28:06 -0500

    standardized print statement

--- CSE4300/abhinav-daniel-eric-vin/rr.c
+++ CSE4300/abhinav-daniel-eric-vin/rr.c
@@ -10,13 +10,11 @@ int tq = timeQuantum; //Keeps track of the remaining time quantum for current pr
 
 void rr(struct process **procArray, int *arrayIdx, int globalTime){
     int turn = counter%(*arrayIdx);    
-    printf("turn at time %d: %s     ", globalTime, procArray[turn]->name);
+    printf("Time %d: Executing %s  Round Robin", globalTime, procArray[turn]->name);
     
     procArray[turn] -> remainingTime--;   
-    printf("Process remaining time: %d  Used Round Robin\n", procArray[turn] -> remainingTime);
 
     if(procArray[turn] -> remainingTime <= 0){
-        printf("%s finished at time %d\n", procArray[turn]->name, globalTime+1);
         procArray[turn] -> finishTime = globalTime+1;
         
         for(int i = turn + 1; i < (*arrayIdx); i++){
