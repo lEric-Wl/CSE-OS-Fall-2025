bb1453f1fef55adfcaeb540125c9b37bb15b6d61
zerolexi <thealexandria0@gmail.com>
2025-11-15 17:31:55 -0500

    finish memory simulation implementation
    added ability to change MemoryManager parameters

--- CSE4300/alex/MemorySimulation.cpp
+++ CSE4300/alex/MemorySimulation.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <limits>
 #include "MemoryManager.h"
 
 MemoryManager mm;
@@ -7,7 +8,7 @@ MemoryManager mm;
 bool running = true;
 
 std::string listOptions() {
-    return "1. Allocate a new page\n2. Delete a page at an address\n3. Write to an address\n4. Read from an address\n5. Print information about the page at an address\n6. Exit\n";
+    return "1. Allocate a new page\n2. Delete a page at an address\n3. Write to an address\n4. Read from an address\n5. Print information about the page at an address\n6. [ADVANCED] Reinitialize MemoryManager\n7. Exit\n";
 }
 
 int hexStringToInt(std::string string) {
@@ -19,15 +20,28 @@ int hexStringToInt(std::string string) {
         if (!isxdigit(character)) return -1;
     }
 
-    int integer = std::stoi(string, nullptr, 16);
+    int integer;
+    try {
+        integer = std::stoi(string, nullptr, 16);
+    } catch (const std::exception& e) {
+        std::cerr << "Caught an exception: " << e.what() << " (probably too big of a number)" << std::endl;
+        return -1;
+    }
 
     return integer;
 }
 
 void allocateAPage() {
-    int newPage = mm.allocateAnyPage();
-    std::string zeroBase = "";
-    if (newPage == 0) zeroBase = "0x";
+    int newPage;
+    try {
+        newPage = mm.allocateAnyPage();
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Caught an exception: " << e.what() << std::endl;
+        return;
+    }
+
+    std::string zeroBase = ""; if (newPage == 0) zeroBase = "0x";
     std::cout << "Your new page is located at virtual memory address: " << zeroBase << std::showbase << std::hex << newPage << std::endl;
     std::cout << "It can be written to from addresses: [" << zeroBase << newPage << ":" << newPage + 4095 << "]" << std::endl;
 }
@@ -36,22 +50,145 @@ void deleteAPage() {
     std::string input;
     int address;
 
-    std::cout << "Enter address of page you would like deleted (enter -1 to return): ";
+    std::cout << "Enter address of page you would like deleted (enter -1 to return to menu): ";
     std::cin >> input; std::cout << std::endl;
 
     address = hexStringToInt(input);
     if (address < 0) {std::cout << "Please enter a valid address!" << std::endl; return;}
 
-    mm.deletePageTableEntry(address);
+    try {
+        mm.deletePageTableEntry(address);
+    } catch (const std::exception& e) {
+        std::cerr << "Caught an exception: " << e.what() << std::endl;
+        return;
+    }
 
     std::cout << "Entry successfully deleted!" << std::endl;
 }
 
-void writeToAnAddress() {}
+void writeToAnAddress() {
+    std::string input;
+    int address;
+    int data;
 
-void readFromAnAddress() {}
+    std::cout << "Enter address you would like to write to (enter -1 to return to menu): ";
+    std::cin >> input; std::cout << std::endl;
 
-void printPageInfo() {}
+    address = hexStringToInt(input);
+    std::string zeroBaseAddr = ""; if (address == 0) zeroBaseAddr = "0x";
+    if (address < 0) {std::cout << "Please enter a valid address!" << std::endl; return;}
+
+    std::cout << "Enter data you would like to write. \nData must be DEC 0-255 or HEX 0x0-0xff (please include the '0x' base!) (enter -1 to return to menu): ";
+    std::cin >> input; std::cout << std::endl;
+    std::string zeroBaseData = "";
+    if (input.compare(0, 2, "0x") == 0 || input.compare(0, 2, "0X") == 0) {
+        data = hexStringToInt(input);
+        std::cout << std::hex << std::showbase;
+        if (data == 0) zeroBaseData = "0x";
+    } else {
+        for (auto character : input){
+            if (!isdigit(character)) data = -1;
+        }
+        data = std::stoi(input, nullptr, 10);
+        if (data > 255) {std::cout << "Please enter valid data!" << std::endl; return;}
+    }
+
+    if (data < 0) {std::cout << "Please enter valid data!" << std::endl; return;}
+
+    try {
+        mm.writeVirtualMemory(address, data);
+    } catch (const std::exception& e) {
+        std::cerr << "Caught an exception: " << e.what() << std::endl;
+        return;
+    }
+
+    std::cout << "Data [" << zeroBaseData << data << "] successfully written to address " << std::hex << std::showbase << zeroBaseAddr << address << "!" << std::endl;
+}
+
+void readFromAnAddress() {
+    std::string input;
+    int address;
+    int data;
+
+    std::cout << "Enter address you would like to write to (enter -1 to return to menu): ";
+    std::cin >> input; std::cout << std::endl;
+
+    address = hexStringToInt(input);
+    if (address < 0) {std::cout << "Please enter a valid address!" << std::endl; return;}
+    std::string zeroBaseAddr = ""; if (address == 0) zeroBaseAddr = "0x";
+
+
+    try {
+        data = mm.readVirtualMemory(address);
+    } catch (const std::exception& e) {
+        std::cerr << "Caught an exception: " << e.what() << std::endl;
+        return;
+    }
+
+    std::string zeroBaseData = ""; if (data == 0) zeroBaseData = "0x";
+
+    std::cout << "Data successfully read!" << std::endl;
+    std::cout << "Value at address " << std::hex << std::showbase << zeroBaseAddr << address << ": " << zeroBaseData << data <<std::endl;
+}
+
+void printPageInfo() {
+    std::string input;
+    int address;
+
+    std::cout << "Enter address you would like to print page info of (enter -1 to return to menu): ";
+    std::cin >> input; std::cout << std::endl;
+
+    address = hexStringToInt(input);
+    if (address < 0) {std::cout << "Please enter a valid address!" << std::endl; return;}
+    std::string zeroBaseAddr = ""; if (address == 0) zeroBaseAddr = "0x";
+
+    std::cout << "Page data at address " << zeroBaseAddr << address << ": " << std::endl;
+
+    try {
+        mm.printPageTableEntry(address);
+    } catch (const std::exception& e) {
+        std::cerr << "Caught an exception: " << e.what() << std::endl;
+        return;
+    }
+
+    std::cout << std::endl;
+}
+
+void reinitializeMemory() {
+    std::string input;
+    int page_size = 4096;
+    int num_pages = 1024;
+    int num_frames = 1024;
+
+
+    std::cout << "WARNING! This will reset all data entered. Enter 1 to continue, -1 to return: ";
+    std::cin >> input; std::cout << std::endl;
+    int confirm;
+
+    try {
+        confirm = std::stoi(input, nullptr, 10);
+    } catch (...) {
+        confirm = -1;
+    }
+
+    if(confirm != 1) return;
+
+    std::cout << "Enter size of each page in bytes (multiple of 2) (enter -1 to return to menu): ";
+    std::cin >> input; std::cout << std::endl;
+    try {page_size = std::stoi(input, nullptr, 10);} catch (...) {page_size = -1;} if(page_size < 0) return;
+
+    std::cout << "Enter number of pages (enter -1 to return to menu): ";
+    std::cin >> input; std::cout << std::endl;
+    try {num_pages = std::stoi(input, nullptr, 10);} catch (...) {num_pages = -1;} if(num_pages < 0) return;
+
+    std::cout << "Enter number of physical memory frames (enter -1 to return to menu): ";
+    std::cin >> input; std::cout << std::endl;
+    try {num_frames = std::stoi(input, nullptr, 10);} catch (...) {num_frames = -1;} if(num_frames < 0) return;
+
+    mm = MemoryManager(page_size, num_pages, num_frames);
+
+    std::cout << "MemoryManager reinitialized with:\n" << num_pages << " " << page_size << "B pages\nNumber of physical memory frames: " << num_frames << std::endl;
+}
 
 void exitProgram() {
      running = false;
@@ -77,11 +214,18 @@ void handleOptions(int choice) {
             printPageInfo();
             break;
         case 6:
+            reinitializeMemory();
+            break;
+        case 7:
             exitProgram();
             break;
     }
-
-    std::cout << std::endl;
+    if (choice != 7) {
+        std::cout << "Press enter to continue...";
+        while (std::cin.get() != '\n');
+        while (std::cin.get() != '\n');
+        std::cout << std::endl;
+    }
 }
 
 int main() {
@@ -99,7 +243,7 @@ int main() {
 
         if (selection.length() == 1 && isdigit(selection[0])) {
             choice = selection[0] - '0';
-            if (choice >= 1 && choice <= 6) {
+            if (choice >= 1 && choice <= 7) {
                 handleOptions(choice);
                 continue;
             }
